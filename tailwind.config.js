f9cd8472769e3cc01d71538dbb59c8656e37b065
krystiano13 <crystianotv@gmail.com>
2023-12-05 12:37:09 +0100

    create basic styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,19 @@ module.exports = {
   ],
   theme: {
     extend: {
+      width: {
+        102: "24rem",
+        103: "28rem"
+      },
+      height: {
+        102: "42rem",
+        103: "46rem"
+      },
       colors: {
         softBlue: "hsl(215, 51%, 70%)",
         cyan: "hsl(178, 100%, 50%)",
         veryDarkBlue: {
+          50:"hsl(215,57%,10%)",
           100: "hsl(217, 54%, 11%)",
           200: "hsl(216, 50%, 16%)",
           300: "hsl(215, 32%, 27%)"
